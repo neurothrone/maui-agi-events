94f42f187889ec48390034ad879242e9f63ba1bc
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-08 16:30:05 +0100

    add CommunityToolkit NuGet and initialize it

--- AGIEvents.App/MauiProgram.cs
+++ AGIEvents.App/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace AGIEvents.App;
 
@@ -9,11 +10,15 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            // Initialize CommunityToolkit
+            .UseMauiCommunityToolkit()
+            // Initialize Services
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+            })
+            ;
 
 #if DEBUG
         builder.Logging.AddDebug();
