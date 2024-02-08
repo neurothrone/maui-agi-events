01a8657e05e2a06275d138ec3f8aad1c86a176fc
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-08 06:37:15 +0100

    add a Lead model

--- AGIEvents.App/Src/Models/Lead.cs
+++ AGIEvents.App/Src/Models/Lead.cs
@@ -2,5 +2,19 @@ namespace AGIEvents.App.Models;
 
 public class Lead
 {
-    
+    public string id;
+    public string eventId;
+    public string firstName;
+    public string lastName;
+    public string company;
+    public string email;
+    public string phone;
+    public string position;
+    public string countryCode;
+    public string address;
+    public string zipCode;
+    public string city;
+    public string product;
+    public string seller;
+    public DateTime scannedAt;
 }
