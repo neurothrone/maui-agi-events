a31a26b0c9e39783ef020dc0f75e5aaf8040d323
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-11 12:42:50 +0100

    add backing field for Events and set up loading

--- AGIEvents.Lib/ViewModels/EventsViewModel.cs
+++ AGIEvents.Lib/ViewModels/EventsViewModel.cs
@@ -1,21 +1,41 @@
 using System.Collections.ObjectModel;
 using AGIEvents.Lib.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace AGIEvents.Lib.ViewModels;
 
-public partial class EventsViewModel
+public partial class EventsViewModel : ObservableObject
 {
-    public ObservableCollection<EventViewModel> Events { get; }
+    [ObservableProperty] private bool _isLoading;
+
+    private ObservableCollection<EventViewModel> _events;
+
+    public ObservableCollection<EventViewModel> Events
+    {
+        get => _events;
+        set => SetProperty(ref _events, value);
+    }
 
     public EventsViewModel()
     {
+        _events = [];
+        FetchEvents();
+    }
+
+    private async void FetchEvents()
+    {
+        IsLoading = true;
+        await Task.Delay(500);
+
         Events = new ObservableCollection<EventViewModel>(
             Event.Samples()
                 .Select(e => new EventViewModel(e))
                 .OrderBy(e => e.Id)
                 .ToList()
         );
+
+        IsLoading = false;
     }
 
     [RelayCommand]
