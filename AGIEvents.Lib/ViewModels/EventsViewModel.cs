fbd1748d5b832a3cf5a919ec34c1b0972192b297
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-11 15:17:50 +0100

    redesign UI to use horizontal linear collections and refactor vm

--- AGIEvents.Lib/ViewModels/EventsViewModel.cs
+++ AGIEvents.Lib/ViewModels/EventsViewModel.cs
@@ -1,26 +1,126 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using AGIEvents.Lib.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace AGIEvents.Lib.ViewModels;
 
-public partial class EventsViewModel : ObservableObject
+public partial class EventsViewModel : ObservableObject, IDisposable
 {
     [ObservableProperty] private bool _isLoading;
 
-    private ObservableCollection<EventViewModel> _events;
+    private ObservableCollection<EventViewModel> _yourEvents;
 
-    public ObservableCollection<EventViewModel> Events
+    public ObservableCollection<EventViewModel> YourEvents
     {
-        get => _events;
-        set => SetProperty(ref _events, value);
+        get => _yourEvents;
+        set => SetProperty(ref _yourEvents, value);
     }
+
+    private ObservableCollection<EventViewModel> _comingEvents;
+
+    public ObservableCollection<EventViewModel> ComingEvents
+    {
+        get => _comingEvents;
+        set => SetProperty(ref _comingEvents, value);
+    }
+
+    public bool IsYourEventsEmpty => !IsLoading && YourEvents.Count == 0;
+    public bool IsComingEventsEmpty => !IsLoading && ComingEvents.Count == 0;
 
     public EventsViewModel()
     {
-        _events = [];
+        _yourEvents = [];
+        _comingEvents = [];
+
+        SubscribeToEvents();
         FetchEvents();
+    }
+
+    void IDisposable.Dispose()
+    {
+        UnsubscribeToEvents();
+    }
+
+    private void SubscribeToEvents()
+    {
+        PropertyChanged += OnYourEventsIsChanged;
+        PropertyChanged += OnComingEventsIsChanged;
+    }
+
+    private void UnsubscribeToEvents()
+    {
+        PropertyChanged -= OnYourEventsIsChanged;
+        PropertyChanged -= OnComingEventsIsChanged;
+    }
+
+    private void OnYourEventsIsChanged(
+        object? sender,
+        PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(IsLoading) or nameof(YourEvents))
+            OnPropertyChanged(nameof(IsYourEventsEmpty));
+    }
+
+    private void OnComingEventsIsChanged(
+        object? sender,
+        PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(IsLoading) or nameof(ComingEvents))
+            OnPropertyChanged(nameof(IsComingEventsEmpty));
+    }
+
+
+    [RelayCommand]
+    private async Task AddEvent()
+    {
+        YourEvents.Add(
+            new EventViewModel(new Event(
+                    id: "2",
+                    title: "Sign, Print & Promotion",
+                    image: "sopno_logo.png",
+                    startDate: DateTime.Now.AddDays(150),
+                    endDate: DateTime.Now.AddDays(152)
+                )
+            )
+        );
+        OnPropertyChanged(nameof(YourEvents));
+    }
+
+    [RelayCommand]
+    private async Task ClearEvents()
+    {
+        YourEvents.Clear();
+        OnPropertyChanged(nameof(YourEvents));
+    }
+
+    [RelayCommand]
+    private async Task AddEvents()
+    {
+        IsLoading = true;
+        await Task.Delay(500);
+
+        YourEvents = new ObservableCollection<EventViewModel>(
+            [
+                new EventViewModel(new Event(
+                    id: "0",
+                    title: "Sign&Print Scandinavia",
+                    image: "sopse_logo.png",
+                    startDate: DateTime.Now.AddDays(100),
+                    endDate: DateTime.Now.AddDays(103)
+                )),
+                new EventViewModel(new Event(
+                    id: "1",
+                    title: "Sign Print & Pack Denmark",
+                    image: "sopdk_logo.png",
+                    startDate: DateTime.Now.AddDays(125),
+                    endDate: DateTime.Now.AddDays(127)
+                ))
+            ]
+        );
+
+        IsLoading = false;
     }
 
     private async void FetchEvents()
@@ -28,11 +128,73 @@ public partial class EventsViewModel : ObservableObject
         IsLoading = true;
         await Task.Delay(500);
 
-        Events = new ObservableCollection<EventViewModel>(
-            Event.Samples()
-                .Select(e => new EventViewModel(e))
-                .OrderBy(e => e.Id)
-                .ToList()
+        // YourEvents = new ObservableCollection<EventViewModel>(
+        //     [
+        //         new EventViewModel(new Event(
+        //             id: "0",
+        //             title: "Sign&Print Scandinavia",
+        //             image: "sopse_logo.png",
+        //             startDate: DateTime.Now.AddDays(100),
+        //             endDate: DateTime.Now.AddDays(103)
+        //         )),
+        //         new EventViewModel(new Event(
+        //             id: "1",
+        //             title: "Sign Print & Pack Denmark",
+        //             image: "sopdk_logo.png",
+        //             startDate: DateTime.Now.AddDays(125),
+        //             endDate: DateTime.Now.AddDays(127)
+        //         ))
+        //     ]
+        // );
+
+        ComingEvents = new ObservableCollection<EventViewModel>(
+            [
+                new EventViewModel(
+                    new Event(
+                        id: "3",
+                        title: "Sign, Print & Promotion",
+                        image: "sopno_logo.png",
+                        startDate: DateTime.Now.AddDays(165),
+                        endDate: DateTime.Now.AddDays(168)
+                    )
+                ),
+                new EventViewModel(
+                    new Event(
+                        id: "3",
+                        title: "Sign, Print & Promotion",
+                        image: "sopno_logo.png",
+                        startDate: DateTime.Now.AddDays(165),
+                        endDate: DateTime.Now.AddDays(168)
+                    )
+                ),
+                new EventViewModel(
+                    new Event(
+                        id: "3",
+                        title: "Sign, Print & Promotion",
+                        image: "sopno_logo.png",
+                        startDate: DateTime.Now.AddDays(165),
+                        endDate: DateTime.Now.AddDays(168)
+                    )
+                ),
+                new EventViewModel(
+                    new Event(
+                        id: "3",
+                        title: "Sign, Print & Promotion",
+                        image: "sopno_logo.png",
+                        startDate: DateTime.Now.AddDays(165),
+                        endDate: DateTime.Now.AddDays(168)
+                    )
+                ),
+                new EventViewModel(
+                    new Event(
+                        id: "3",
+                        title: "Sign, Print & Promotion",
+                        image: "sopno_logo.png",
+                        startDate: DateTime.Now.AddDays(165),
+                        endDate: DateTime.Now.AddDays(168)
+                    )
+                ),
+            ]
         );
 
         IsLoading = false;
