a2bd4263c76cfb82eaa346ffa18901b2dac1c1ad
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-19 04:07:45 +0100

    refactor and add factory constructor for LeadRecord

--- AGIEvents.Lib/ViewModels/LeadViewModel.cs
+++ AGIEvents.Lib/ViewModels/LeadViewModel.cs
@@ -1,16 +1,50 @@
-using AGIEvents.Lib.Models;
+using AGIEvents.Lib.Services.Database.DTO;
 
 namespace AGIEvents.Lib.ViewModels;
 
-public partial class LeadViewModel(Lead lead)
+public partial class LeadViewModel
 {
-    public string Id => lead.Id;
-    public string EventId => lead.EventId;
-    public string FirstName => lead.FirstName;
-    public string LastName => lead.LastName;
+    public int LeadId { get; }
+    public string EventId { get; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string Company { get; }
+    public string Email { get; }
+    public string Phone { get; }
+    public DateTime ScannedDate { get; }
+
     public string FullName => $"{FirstName} {LastName}";
-    public string Company => lead.company;
-    public string Email => lead.email;
-    public string Phone => lead.phone;
-    public DateTime ScannedAt => lead.scannedAt;
+
+    public LeadViewModel(
+        int leadId,
+        string eventId,
+        string firstName,
+        string lastName,
+        string company,
+        string email,
+        string phone,
+        DateTime scannedDate)
+    {
+        LeadId = leadId;
+        EventId = eventId;
+        FirstName = firstName;
+        LastName = lastName;
+        Company = company;
+        Email = email;
+        Phone = phone;
+        ScannedDate = scannedDate;
+    }
+
+    public static LeadViewModel FromRecord(LeadRecord record)
+    {
+        return new LeadViewModel(
+            record.LeadId,
+            record.EventId,
+            record.FirstName,
+            record.LastName,
+            record.Company,
+            record.Email,
+            record.Phone,
+            record.ScannedDate);
+    }
 }
