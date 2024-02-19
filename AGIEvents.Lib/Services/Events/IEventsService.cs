97c9d702fd4e8be486ce7e9a469edfb7d21b7904
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-19 06:33:25 +0100

    update type name

--- AGIEvents.Lib/Services/Events/IEventsService.cs
+++ AGIEvents.Lib/Services/Events/IEventsService.cs
@@ -1,9 +1,9 @@
-using AGIEvents.Lib.Services.Database.DTO;
+using AGIEvents.Lib.Domain;
 
 namespace AGIEvents.Lib.Services.Events;
 
 // We will use this to load events from a file. Someday we might to load the events from Firebase and images from Storage
 public interface IEventsService
 {
-    Task<EventRecord[]> LoadEvents();
+    Task<EventRecordDto[]> LoadEvents();
 }
