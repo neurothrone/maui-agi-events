6c9407d3d6677462ce96364b1417c9efc4c17991
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-19 03:06:35 +0100

    update columns with nameof

--- AGIEvents.Lib/Services/Database/Domain/EventEntity.cs
+++ AGIEvents.Lib/Services/Database/Domain/EventEntity.cs
@@ -2,13 +2,13 @@ using SQLite;
 
 namespace AGIEvents.Lib.Services.Database.Domain;
 
-[Table("Events")]
+[Table(nameof(EventEntity))]
 public class EventEntity
 {
-    [PrimaryKey] [Column("EventId")] public string EventId { get; init; }
+    [PrimaryKey] [Column(nameof(EventId))] public string EventId { get; init; }
 
-    [Column("Title")] public string Title { get; init; }
-    [Column("Image")] public string Image { get; init; }
-    [Column("StartDate")] [Indexed] public DateTime StartDate { get; init; }
-    [Column("EndDate")] public DateTime EndDate { get; init; }
+    [Column(nameof(Title))] public string Title { get; init; }
+    [Column(nameof(Image))] public string Image { get; init; }
+    [Column(nameof(StartDate))] [Indexed] public DateTime StartDate { get; init; }
+    [Column(nameof(EndDate))] public DateTime EndDate { get; init; }
 }
--- AGIEvents.Lib/Services/Database/Domain/LeadEntity.cs
+++ AGIEvents.Lib/Services/Database/Domain/LeadEntity.cs
@@ -2,22 +2,33 @@ using SQLite;
 
 namespace AGIEvents.Lib.Services.Database.Domain;
 
-[Table("Leads")]
+[Table(nameof(LeadEntity))]
 public class LeadEntity
 {
     [PrimaryKey, AutoIncrement]
-    [Column("LeadId")]
+    [Column(nameof(LeadId))]
     public int LeadId { get; init; }
 
-    [Column("EventId")] [Indexed] [Unique] public string EventId { get; init; }
+    [Column(nameof(EventId))]
+    [Indexed]
+    public string EventId { get; init; }
 
-    [Column("Title")] public string FirstName { get; init; }
-    [Column("Title")] public string LastName { get; init; }
-    [Column("Title")] public string Company { get; init; }
-    [Column("Title")] public string Email { get; init; }
-    [Column("Title")] public string Phone { get; init; }
-    [Column("Title")] public string Address { get; init; }
-    [Column("Title")] public string ZipCode { get; init; }
-    [Column("Title")] public string City { get; init; }
-    [Column("ScannedDate")] [Indexed] public DateTime ScannedDate { get; init; }
+    [Column(nameof(FirstName))] public string FirstName { get; init; }
+    [Column(nameof(LastName))] public string LastName { get; init; }
+    [Column(nameof(Company))] public string Company { get; init; }
+    [Column(nameof(Email))] public string Email { get; init; }
+    [Column(nameof(Phone))] public string Phone { get; init; }
+    [Column(nameof(Address))] public string Address { get; init; }
+    [Column(nameof(ZipCode))] public string ZipCode { get; init; }
+    [Column(nameof(City))] public string City { get; init; }
+    [Column(nameof(Product))] public string Product { get; init; }
+    [Column(nameof(Seller))] public string Seller { get; init; }
+
+    // TODO: should we specify a MaxLength?
+    // [Column(nameof(Notes)), MaxLength(10_000)] public string Notes { get; init; }
+    [Column(nameof(Notes))] public string Notes { get; init; }
+
+    [Column(nameof(ScannedDate))]
+    [Indexed]
+    public DateTime ScannedDate { get; init; }
 }
