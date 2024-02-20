2f115877245026c77a325b175755a9b49472d052
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-20 10:25:26 +0100

    add json converter for phones when value in json is not a string

--- AGIEvents.Lib/Services/Firebase/Domain/Participant.cs
+++ AGIEvents.Lib/Services/Firebase/Domain/Participant.cs
@@ -1,13 +1,17 @@
 using System.Text.Json.Serialization;
+using AGIEvents.Lib.Services.Firebase.Utility;
 
 namespace AGIEvents.Lib.Services.Firebase.Domain;
 
 public abstract class Participant
 {
     [JsonPropertyName("exhibitionId")] public string ExhibitionId { get; init; }
-    [JsonPropertyName("firstName")] public string FirstName { get; init; }
-    [JsonPropertyName("lastName")] public string LastName { get; init; }
-    [JsonPropertyName("company")] public string Company { get; init; }
-    [JsonPropertyName("email")] public string Email { get; init; }
-    [JsonPropertyName("phone")] public string Phone { get; init; }
+    [JsonPropertyName("firstName")] public string FirstName { get; set; }
+    [JsonPropertyName("lastName")] public string LastName { get; set; }
+    [JsonPropertyName("company")] public string Company { get; set; }
+    [JsonPropertyName("email")] public string Email { get; set; }
+
+    [JsonConverter(typeof(PhoneJsonConverter))]
+    [JsonPropertyName("phone")]
+    public string Phone { get; set; }
 }
