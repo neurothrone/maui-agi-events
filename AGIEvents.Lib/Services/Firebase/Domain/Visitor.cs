021c538a28de3fd00913f181a7331f58c8ab15b5
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-20 10:25:47 +0100

    add factory constructor to create from Exhibitor

--- AGIEvents.Lib/Services/Firebase/Domain/Visitor.cs
+++ AGIEvents.Lib/Services/Firebase/Domain/Visitor.cs
@@ -8,4 +8,17 @@ public class Visitor : Participant
     [JsonPropertyName("addressField")] public string Address { get; set; }
     [JsonPropertyName("zipcode")] public string ZipCode { get; set; }
     [JsonPropertyName("city")] public string City { get; set; }
+
+    public static Visitor FromExhibitor(Exhibitor exhibitor)
+    {
+        var visitor = new Visitor
+        {
+            FirstName = exhibitor.FirstName,
+            LastName = exhibitor.LastName,
+            Company = exhibitor.Company,
+            Email = exhibitor.Email,
+            Phone = exhibitor.Phone,
+        };
+        return visitor;
+    }
 }
