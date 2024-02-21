3496b5db41ee60e997321ea7ce4d5d88f66c6f8d
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-21 10:40:42 +0100

    complete about page

--- AGIEvents.Lib/ViewModels/AboutViewModel.cs
+++ AGIEvents.Lib/ViewModels/AboutViewModel.cs
@@ -1,5 +1,29 @@
+using AGIEvents.Lib.Services;
+using CommunityToolkit.Mvvm.Input;
+
 namespace AGIEvents.Lib.ViewModels;
 
-public class AboutViewModel
+public partial class AboutViewModel(
+    IAppInteractionsService appInteractionsService,
+    IConfigurationService configurationService)
 {
+    private const string LinkedinUrl = "https://www.linkedin.com/in/neurothrone/";
+
+    public string AppTitle => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string Author => "Zane Neurothrone";
+    public string DotNetTextCredits => "This app is powered by .NET MAUI";
+    public string DotNetDetailCredits => "Written in XAML & C#";
+
+    [RelayCommand]
+    private async Task OpenSupportEmailLink()
+    {
+        await appInteractionsService.ComposeEmailAsync(
+            configurationService.GetSupportEmail(),
+            "GasMatic Support"
+        );
+    }
+
+    [RelayCommand]
+    private async Task OpenLinkedinWebLink() => await appInteractionsService.OpenBrowserAsync(LinkedinUrl);
 }
