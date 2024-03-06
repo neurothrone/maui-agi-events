a6e0193d0c24f7dbf7c64b2ce49f4eb495fac2d6
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-03-06 02:27:13 +0100

    remove entry underline for android

--- AGIEvents.App/App.xaml.cs
+++ AGIEvents.App/App.xaml.cs
@@ -1,11 +1,22 @@
 ﻿namespace AGIEvents.App;
+#if ANDROID
+using Android.Content.Res;
+#endif
 
 public partial class App : Application
 {
     public App()
     {
-        // TODO: Dark Theme
         Application.Current.UserAppTheme = AppTheme.Dark;
+
+        // Remove underline from Android Entry control
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, _) =>
+        {
+#if ANDROID
+            handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#endif
+        });
+
         InitializeComponent();
 
         MainPage = new AppShell();
